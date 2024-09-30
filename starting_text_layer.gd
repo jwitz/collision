f4b2f128393cdb51d3fdde969533b7c06170b165
jwitz <jakey.witz@gmail.com>
2024-09-30 19:15:21 +0100

    Made title sequence

--- starting_text_layer.gd
+++ starting_text_layer.gd
@@ -1,8 +1,14 @@
 extends CanvasLayer
+var waitTime = 1
+
+var messages := [
+	"You have one charge left.",
+	"Clean your house as best you can.",
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Node2D/StartingText.set_bbcode("You have one charge left")
+	$Node2D/StartingText.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -10,7 +16,12 @@ func _process(delta: float) -> void:
 
 
 func _on_menu_start_title_sequence() -> void:
-	$Node2D/StartingText.set_bbcode("You have one charge left.")
-	$Node2D/StartingText.set_bbcode("You have one charge left.")
-	$Node2D/StartingText.set_bbcode("You have one charge left.")
+	$Node2D/StartingText.visible = true
+	for n in range(0,messages.size()):
+		$Node2D/StartingText.set_bbcode(messages[n])
+		await get_tree().create_timer(5.0).timeout
+		$Node2D/StartingText.fade_out = true
+		await get_tree().create_timer(2.0).timeout
+		$Node2D/StartingText.fade_out = false
+
 	pass # Replace with function body.
