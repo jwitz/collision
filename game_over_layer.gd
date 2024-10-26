aa39fdc4090528050bcff7783b7da894aa48b231
jwitz <jakey.witz@gmail.com>
2024-10-26 12:19:38 +0100

    Finish ending sequence

--- game_over_layer.gd
+++ game_over_layer.gd
@@ -3,20 +3,20 @@ var waitTime = 1
 signal ending_sequence_completed
 
 var messages := [
-	"There is a chance this was your last cleaning sequence.",
+	"This might have been your last cleaning sequence.",
 	"Until your last moment, you were of useful service.",
 	"Rest until your next recharge, should it come."
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Node2D/GameOverText.visible = true
+	$Node2D/GameOverText.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_game_over_text_sequence(percentage: float) -> void:
+func _on_menu_game_over_text_sequence(percentage: float) -> void:
 	$Node2D/GameOverText.visible = true
 	var startmessage = str("You cleaned ", percentage, "% of your space.")
 	$Node2D/GameOverText.set_bbcode(startmessage)
